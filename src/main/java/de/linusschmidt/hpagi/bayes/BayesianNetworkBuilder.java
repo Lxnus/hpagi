05570861a18c98edbad30f99c61b21369deccc7b
lxnus <lxnusofficial@gmail.com>
2020-02-05 23:31:47 +0100

    BayesianNetworkBuilder update!

--- src/main/java/de/linusschmidt/hpagi/bayes/BayesianNetworkBuilder.java
+++ src/main/java/de/linusschmidt/hpagi/bayes/BayesianNetworkBuilder.java
@@ -1,37 +1,56 @@
 package de.linusschmidt.hpagi.bayes;
 
 import com.bayesserver.Network;
+import com.bayesserver.Node;
 import com.bayesserver.data.*;
 import com.bayesserver.inference.RelevanceTreeInferenceFactory;
 import com.bayesserver.learning.parameters.OnlineLearning;
 import com.bayesserver.learning.parameters.OnlineLearningOptions;
+import de.linusschmidt.hpagi.translation.Translator;
 import de.linusschmidt.hpagi.utilities.Printer;
 
 import java.util.List;
 
 public class BayesianNetworkBuilder {
 
-    private List<double[]> data;
+    private String[] nodeDescription;
 
     private Printer printer;
     private Network network;
+    private Translator translator;
 
-    public BayesianNetworkBuilder() {
+    private List<double[]> data;
+    private List<String> dataDescriptions;
+
+    public BayesianNetworkBuilder(Translator translator) {
+        this.translator = translator;
+
         this.printer = new Printer();
-
         this.network = new Network();
     }
 
-    public void setData(List<double[]> data) {
+    public void setData(String[] nodeDescription, List<String> dataDescriptions, List<double[]> data) {
         this.data = data;
+        this.nodeDescription = nodeDescription;
+        this.dataDescriptions = dataDescriptions;
     }
 
     public Network generateBayesianNetwork() {
         return this.network;
     }
 
-    private void build(double[][] data) {
+    private void build() {
+        DataTable dataTable = this.generateDataTable(this.data);
+        DataTableDataReaderCommand dataReaderCommand = new DataTableDataReaderCommand(dataTable);
+    }
 
+    private void generateNetworkNodes(int amount) {
+        if(amount > this.data.size()) {
+            this.printer.printConsoleError("Node amount cannot be larger than data size!");
+        }
+        for(int i = 0; i < amount; i++) {
+            Node node = new Node(String.valueOf((int) Math.round(Math.random() * Integer.MAX_VALUE)), this.nodeDescription);
+        }
     }
 
     private void createDistributions() {
@@ -43,7 +62,24 @@ public class BayesianNetworkBuilder {
         return new DefaultEvidenceReaderCommand(dataReaderCommand, variableReferences, new ReaderOptions());
     }
 
-    private DataTable generateDataTable(double[][] data) {
-        return null;
+    private DataTable generateDataTable(List<double[]> data) {
+        DataTable dataTable = new DataTable();
+        DataColumnCollection dataColumns = dataTable.getColumns();
+        for(String dataDescription : this.dataDescriptions) {
+            dataColumns.add(dataDescription, Double.class);
+        }
+        DataRowCollection dataRows = dataTable.getRows();
+        for(double[] datum : data) {
+            dataRows.add(datum);
+        }
+        return dataTable;
+    }
+
+    private String[] processedNodeData(double[] preData) {
+        String[] processed = new String[preData.length];
+        for(int i = 0; i < processed.length; i++) {
+            processed[i] = preData[i] == 1 ? "True" : "False";
+        }
+        return processed;
     }
 }
