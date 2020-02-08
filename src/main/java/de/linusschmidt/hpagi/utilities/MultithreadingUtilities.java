8e21b93091b5c0eb85dee6e884fe91927e0433c7
lxnus <lxnusofficial@gmail.com>
2020-02-08 15:38:48 +0100

    Code update!

--- src/main/java/de/linusschmidt/hpagi/utilities/MultithreadingUtilities.java
+++ src/main/java/de/linusschmidt/hpagi/utilities/MultithreadingUtilities.java
@@ -8,7 +8,7 @@ public class MultithreadingUtilities {
     public static List<List<double[]>> partition(int threads, List<double[]> data) {
         List<List<double[]>> partition = new ArrayList<>();
         int elementsPerThread = (data.size() >= threads) ? (data.size() / threads) : 1;
-        int effectiveParallelism = (data.size() >= threads) ? threads : data.size();
+        int effectiveParallelism = Math.min(data.size(), threads);
         for(int iParallelism = 0; iParallelism < effectiveParallelism; iParallelism++) {
             int from = iParallelism * elementsPerThread;
             int to = iParallelism == (effectiveParallelism - 1) ? (data.size()) : ((iParallelism + 1) * elementsPerThread);
