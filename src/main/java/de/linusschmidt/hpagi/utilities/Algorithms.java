ee566e08b780c7aa04d23c93a4199a96fbc7cfb7
lxnus <lxnusofficial@gmail.com>
2020-02-10 22:42:44 +0100

    Code update!

--- src/main/java/de/linusschmidt/hpagi/utilities/Algorithms.java
+++ src/main/java/de/linusschmidt/hpagi/utilities/Algorithms.java
@@ -22,4 +22,23 @@ public class Algorithms {
         }
         return null;
     }
+
+    public LinkedList<Double> markovChain(double[][] transition) {
+        LinkedList<Double> chain = new LinkedList<>();
+        int n = transition.length;
+        int state = n - 1;
+        while(state > 0) {
+            chain.add((double) state);
+            double random = Math.random();
+            double sum = 0.0D;
+            for(int i = 0; i < n; i++) {
+                sum += transition[state][i];
+                if(sum >= random) {
+                    state = i;
+                    break;
+                }
+            }
+        }
+        return chain;
+    }
 }
