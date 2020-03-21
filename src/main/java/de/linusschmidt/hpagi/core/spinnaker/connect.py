df746f7b2811f1b236a0756e1c5e8124d5f1d9b6
lxnus <lxnusofficial@gmail.com>
2020-03-21 16:29:46 +0100

    [SpiNNaker] Kernel update!

--- src/main/java/de/linusschmidt/hpagi/core/spinnaker/connect.py
+++ src/main/java/de/linusschmidt/hpagi/core/spinnaker/connect.py
@@ -7,17 +7,11 @@ token = client.token
 
 newClient = nmpi.Client(username="Lxnus", token=token)
 
-job_id = newClient.submit_job(
+job = newClient.submit_job(
     source="kernel.py",
     platform=nmpi.SPINNAKER,
     collab_id=79910,
     wait=True)
-
-newClient.job_status(job_id=job_id)
-
-job = newClient.get_job(job_id=job_id)
-
-pprint(job)
 
 filenames = newClient.download_data(job=job, local_dir=".")
 
